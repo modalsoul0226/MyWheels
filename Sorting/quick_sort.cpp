9618ecef011c65e155af894b60c912c3f5136cd7
Uyupibu_IU <modalsoul0226@gmail.com>
2018-05-04 14:51:49 -0400

    Initial commit.

---
+++ Sorting/quick_sort.cpp
@@ -0,0 +1,4 @@
+//
+// Created by wang on 18-5-4.
+//
+
---
+++ sorting/quick_sort.cpp
@@ -0,0 +1,11 @@
+//
+// Created by wang on 18-5-4.
+//
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
